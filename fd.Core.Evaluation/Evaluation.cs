e0fc4fedcd7916f3b55b44c264e069b1bd40f13f
arothenhaeuser <andi.rothenhaeuser@gmx.de>
2018-07-06 15:08:39 +0200

    Added Accuracy, Precision, Recall and F1.

--- fd.Core.Evaluation/Evaluation.cs
+++ fd.Core.Evaluation/Evaluation.cs
@@ -50,7 +50,106 @@ namespace fd.Coins.Evaluation
             return (C-(A*B/D))/(((A+B)/2)-(A * B /D));
         }
 
-        public static double NChooseK(int n, int k)
+        public static double Accuracy<T>(IEnumerable<IEnumerable<T>> clustering, IEnumerable<IEnumerable<T>> gold)
+        {
+            var x = clustering.Count();
+            var y = gold.Count();
+            var c1 = clustering.ToArray();
+            var c2 = gold.ToArray();
+            var contingencyTable = new int[x][];
+            for (var i = 0; i < x; i++)
+            {
+                contingencyTable[i] = new int[y];
+            }
+            double TP = 0, TN = 0, FP = 0, FN = 0;
+            for (var i = 0; i < x; i++)
+            {
+                for (var j = 0; j < y; j++)
+                {
+                    contingencyTable[i][j] = c1[i].Intersect(c2[j]).Count();
+                    if (i == j)
+                    {
+                        TP += contingencyTable[i][j];
+                        TN += contingencyTable[i][j];
+                    }
+                    else
+                    {
+                        FN += contingencyTable[i][j];
+                        FP += contingencyTable[i][j];
+                    }
+                }
+            }
+            return (TP + TN) / (TP + TN + FP + FN);
+        }
+
+        public static double Precision<T>(IEnumerable<IEnumerable<T>> clustering, IEnumerable<IEnumerable<T>> gold)
+        {
+            var x = clustering.Count();
+            var y = gold.Count();
+            var c1 = clustering.ToArray();
+            var c2 = gold.ToArray();
+            var contingencyTable = new int[x][];
+            for (var i = 0; i < x; i++)
+            {
+                contingencyTable[i] = new int[y];
+            }
+            double TP = 0, FP = 0;
+            for (var i = 0; i < x; i++)
+            {
+                for (var j = 0; j < y; j++)
+                {
+                    contingencyTable[i][j] = c1[i].Intersect(c2[j]).Count();
+                    if (i == j)
+                    {
+                        TP += contingencyTable[i][j];
+                    }
+                    else
+                    {
+                        FP += contingencyTable[i][j];
+                    }
+                }
+            }
+            return (TP) / (TP + FP);
+        }
+
+        public static double Recall<T>(IEnumerable<IEnumerable<T>> clustering, IEnumerable<IEnumerable<T>> gold)
+        {
+            var x = clustering.Count();
+            var y = gold.Count();
+            var c1 = clustering.ToArray();
+            var c2 = gold.ToArray();
+            var contingencyTable = new int[x][];
+            for (var i = 0; i < x; i++)
+            {
+                contingencyTable[i] = new int[y];
+            }
+            double TP = 0, FN = 0;
+            for (var i = 0; i < x; i++)
+            {
+                for (var j = 0; j < y; j++)
+                {
+                    contingencyTable[i][j] = c1[i].Intersect(c2[j]).Count();
+                    if (i == j)
+                    {
+                        TP += contingencyTable[i][j];
+                    }
+                    else
+                    {
+                        FN += contingencyTable[i][j];
+                    }
+                }
+            }
+            return (TP) / (TP + FN);
+        }
+
+        public static double F1<T>(IEnumerable<IEnumerable<T>> clustering, IEnumerable<IEnumerable<T>> gold)
+        {
+            var p = Precision(clustering, gold);
+            var r = Recall(clustering, gold);
+            return 2 * p * r / (p + r);
+        }
+
+        private static double NChooseK(int n, int k)
         {
             if (n == 0) return 0;
             if (n < k) return 0;
--- fd.Core.Evaluation/Program.cs
+++ fd.Core.Evaluation/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,19 @@ namespace fd.Coins.Evaluation
     {
         static void Main(string[] args)
         {
+            var gold = new List<List<string>>();
+            gold = File.ReadAllLines(@"D:\Source\Repos\CurrentVersion\fd.Coins.AFistfulOfBitcoins\bin\Debug\Clusters.txt").Select(x => x.Split('\t').ToList()).ToList();
+            var c1 = new List<List<string>>();
+            c1 = File.ReadAllLines(@"D:\Source\Repos\CurrentVersion\fd.Coins.AFistfulOfBitcoins\bin\Debug\Clusters - Kopie.txt").Select(x => x.Split('\t').ToList()).ToList();
+
+            Console.WriteLine("RandIndex:\t\t" + Evaluation.RandIndex(c1, gold));
+            Console.WriteLine("AdjustedRandIndex:\t" + Evaluation.AdjustedRandIndex(c1, gold));
+            Console.WriteLine("Accuracy:\t\t" + Evaluation.Accuracy(c1, gold));
+            Console.WriteLine("Precision:\t\t" + Evaluation.Precision(c1, gold));
+            Console.WriteLine("Recall:\t\t\t" + Evaluation.Recall(c1, gold));
+            Console.WriteLine("F1:\t\t\t" + Evaluation.F1(c1, gold));
+
+            Console.Read();
         }
     }
 }

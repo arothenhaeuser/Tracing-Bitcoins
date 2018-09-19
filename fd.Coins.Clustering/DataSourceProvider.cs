49373e51605a354514ab7f1d39fe2103d45af865
Andreas Rothenhäuser <andreas.rothenhaeuser@fast-detect.de>
2018-09-19 12:26:16 +0200

    Added DataSourceProvider.

---
+++ fd.Coins.Clustering/DataSourceProvider.cs
@@ -0,0 +1,100 @@
+﻿using Orient.Client;
+using OrientDB_Net.binary.Innov8tive.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace fd.Coins.Clustering
+{
+    public class DataSourceProvider
+    {
+        private DataSourceOptions _options;
+
+        public DataSourceProvider()
+        {
+            _options = new DataSourceOptions("", LimitType.DEPTH, 8);
+        }
+
+        public DataSourceProvider(string startTx, LimitType type, int limit)
+        {
+            _options = new DataSourceOptions(startTx, type, limit);
+        }
+
+        public List<ORID> GetRids(ConnectionOptions dbOptions)
+        {
+            List<ORID> result = null;
+            using(var db = new ODatabase(dbOptions))
+            {
+                switch (_options.Type)
+                {
+                    case LimitType.DEPTH:
+                        result = db.Query<ODocument>($"TRAVERSE out() FROM (SELECT * FROM Transaction WHERE Hash = '{_options.StartTx}') WHILE $depth <= {_options.Limit} STRATEGY BREADTH_FIRST").Select(x => x.GetField<ORID>("@ORID")).ToList();
+                        break;
+                    case LimitType.DATE:
+                        var start = db.Query<ODocument>($"SELECT BlockTime AS value FROM Transaction WHERE Hash = '{_options.StartTx}'").Select(x => x.GetField<DateTime>("value")).FirstOrDefault();
+                        var end = start.AddDays(_options.Limit);
+                        result = db.Query<ODocument>($"SELECT * FROM Transaction WHERE BlockTime > '{start.ToString("yyyy-MM-dd hh:mm:ss")}' AND BlockTime < '{end.ToString("yyyy-MM-dd hh:mm:ss")}'").ToList().Select(x => x.GetField<ORID>("@ORID")).ToList();
+                        break;
+                    case LimitType.COUNT:
+                        result = db.Query<ODocument>($"TRAVERSE out() FROM (SELECT * FROM Transaction WHERE Hash = '{_options.StartTx}') LIMIT {_options.Limit} STRATEGY BREADTH_FIRST").Select(x => x.GetField<ORID>("@ORID")).ToList();
+                        break;
+                    default:
+                        result = new List<ORID>();
+                        break;
+                }
+                return result;
+            }
+        }
+
+        public List<string> GetAddresses(ConnectionOptions dbOptions)
+        {
+            List<string> result = null;
+            using (var db = new ODatabase(dbOptions))
+            {
+                switch (_options.Type)
+                {
+                    case LimitType.DEPTH:
+                        result = db.Command($"SELECT list(inE().tAddr) as addresses FROM (TRAVERSE out() FROM (SELECT * FROM Transaction WHERE Hash = '{_options.StartTx}') WHILE $depth <= {_options.Limit} STRATEGY BREADTH_FIRST)").ToList().FirstOrDefault().GetField<List<string>>("addresses").Where(x => !string.IsNullOrEmpty(x)).Distinct().ToList();
+                        break;
+                    case LimitType.DATE:
+                        var start = db.Query<ODocument>($"SELECT BlockTime AS value FROM Transaction WHERE Hash = '{_options.StartTx}'").Select(x => x.GetField<DateTime>("value")).FirstOrDefault();
+                        var end = start.AddDays(_options.Limit);
+                        result = db.Command($"SELECT list(inE().tAddr) as addresses FROM Transaction WHERE BlockTime > '{start.ToString("yyyy-MM-dd hh:mm:ss")}' AND BlockTime < '{end.ToString("yyyy-MM-dd hh:mm:ss")}'").ToList().FirstOrDefault().GetField<List<string>>("addresses").Where(x => !string.IsNullOrEmpty(x)).Distinct().ToList();
+                        break;
+                    case LimitType.COUNT:
+                        result = db.Command($"SELECT list(inE().tAddr) as addresses FROM (TRAVERSE out() FROM (SELECT * FROM Transaction WHERE Hash = '{_options.StartTx}') LIMIT {_options.Limit} STRATEGY BREADTH_FIRST)").ToList().FirstOrDefault().GetField<List<string>>("addresses").Where(x => !string.IsNullOrEmpty(x)).Distinct().ToList();
+                        break;
+                    default:
+                        result = new List<string>();
+                        break;
+                }
+                return result;
+            }
+        }
+
+        private long DaysToMs(int days)
+        {
+            return days * 24 * 60 * 60 * 1000;
+        }
+    }
+
+    public class DataSourceOptions
+    {
+        public DataSourceOptions(string startTx, LimitType type, int limit)
+        {
+            StartTx = startTx;
+            Type = type;
+            Limit = limit;
+        }
+        public string StartTx { get; private set; }
+        public LimitType Type { get; private set; }
+        public int Limit { get; private set; }
+    }
+
+    public enum LimitType
+    {
+        DEPTH = 0,
+        DATE = 1,
+        COUNT = 2
+    }
+}
--- fd.Coins.Clustering/Program.cs
+++ fd.Coins.Clustering/Program.cs
@@ -12,62 +12,60 @@ namespace fd.Coins.Clustering
     {
         static void Main(string[] args)
         {
+
             var txgraphOptions = new ConnectionOptions() { DatabaseName = "txgraph", DatabaseType = ODatabaseType.Graph, HostName = "localhost", Password = "admin", Port = 2424, UserName = "admin" };
+            var data = new DataSourceProvider("4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b", LimitType.DATE, 15);
+            var rids = data.GetRids(txgraphOptions);
+            var addresses = data.GetAddresses(txgraphOptions);
+
             var algoPipe = new List<Core.Clustering.Clustering>();
             algoPipe.Add(new TotalAmounts());
             algoPipe.Add(new TimeSlots());
             algoPipe.Add(new Core.Clustering.Intrinsic.DayOfWeek());
-            algoPipe.Add(new Heuristic1());
-            algoPipe.Add(new Heuristic2());
+            //algoPipe.Add(new Heuristic1());
+            //algoPipe.Add(new Heuristic2());
 
-            using (var txgraph = new ODatabase(txgraphOptions))
+
+            //using (var txgraph = new ODatabase(txgraphOptions))
+            //{
+            //    long skip = 0;
+            //    long limit = 50000;
+            //    long total = txgraph.CountRecords;
+
+            //    while (skip < 1000000/*total*/)
+            //    {
+            //        var rids = txgraph.Command($"SELECT @rid FROM Transaction SKIP {skip} LIMIT {limit}").ToList().Select(x => x.GetField<ORID>("rid")).ToList();
+            //        skip += limit;
+
+            var tasks = new List<Task>();
+            foreach (var algo in algoPipe)
             {
-                long skip = 0;
-                long limit = 50000;
-                long total = txgraph.CountRecords;
-
-                while (skip < 1000000/*total*/)
+                tasks.Add(Task.Run(() =>
                 {
-                    var rids = txgraph.Command($"SELECT @rid FROM Transaction SKIP {skip} LIMIT {limit}").ToList().Select(x => x.GetField<ORID>("rid")).ToList();
-                    skip += limit;
-
-                    var tasks = new List<Task>();
-                    foreach(var algo in algoPipe)
-                    {
-                        tasks.Add(Task.Run(() =>
-                        {
-                            algo.Run(txgraphOptions, rids);
-                        }));
-                    }
-                    Task.WaitAll(tasks.ToArray());
-                    Console.WriteLine($"{skip} processed.");
-                }
-
-                var result = new Dictionary<string, double>();
-                var addresses = GetAddresses();
-                foreach(var addr in addresses)
-                {
-                    var distance = 0.0;
-                    foreach (var algo in algoPipe)
-                    {
-                        distance += algo.Distance(addr, "1dicec9k7KpmQaA8Uc8aCCxfWnwEWzpXE");
-                    }
-                    distance /= algoPipe.Count;
-                    result.Add(addr, distance);
-                }
-                Console.WriteLine(string.Join("\n", result.OrderBy(x => x.Value).Select(x => string.Join(",", x.Key, x.Value))));
+                    algo.Run(txgraphOptions, addresses);
+                }));
             }
+            Task.WaitAll(tasks.ToArray());
+            //Console.WriteLine($"{skip} processed.");
+            //}
+
+            var result = new Dictionary<string, double>();
+            foreach (var addr in addresses)
+            {
+                var distance = 0.0;
+                foreach (var algo in algoPipe)
+                {
+                    distance += algo.Distance(addr, addresses[0]);
+                }
+                distance /= algoPipe.Count;
+                result.Add(addr, distance);
+            }
+            Console.WriteLine(string.Join("\n", result.OrderBy(x => x.Value).Select(x => string.Join(",", x.Key, x.Value))));
+            //}
+
+            var debug = result.OrderBy(x => x.Value).ToDictionary(x => x.Key, y => y.Value);
 
             Console.Read();
-        }
-
-        private static List<string> GetAddresses()
-        {
-            var txgraphOptions = new ConnectionOptions() { DatabaseName = "txgraph", DatabaseType = ODatabaseType.Graph, HostName = "localhost", Password = "admin", Port = 2424, UserName = "admin" };
-            using (var txgraph = new ODatabase(txgraphOptions))
-            {
-                return txgraph.Command($"SELECT list(inE().tAddr) AS addresses FROM (SELECT * FROM Transaction SKIP 3772550 LIMIT 500)").ToList().FirstOrDefault().GetField<List<string>>("addresses").Distinct().ToList();
-            }
         }
     }
 }

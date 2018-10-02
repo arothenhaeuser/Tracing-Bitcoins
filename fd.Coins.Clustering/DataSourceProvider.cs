fff74eda42e487d86bada5b67a5a81208e89060c
Andreas Rothenhäuser <andreas.rothenhaeuser@fast-detect.de>
2018-10-02 13:35:53 +0200

    Removed unused function.

--- fd.Coins.Clustering/DataSourceProvider.cs
+++ fd.Coins.Clustering/DataSourceProvider.cs
@@ -20,32 +20,6 @@ namespace fd.Coins.Clustering
             _options = new DataSourceOptions(startTx, type, limit);
         }
 
-        public List<ORID> GetRids(ConnectionOptions dbOptions)
-        {
-            List<ORID> result = null;
-            using(var db = new ODatabase(dbOptions))
-            {
-                switch (_options.Type)
-                {
-                    case LimitType.DEPTH:
-                        result = db.Query<ODocument>($"TRAVERSE out() FROM (SELECT * FROM Transaction WHERE Hash = '{_options.StartTx}') WHILE $depth <= {_options.Limit} STRATEGY BREADTH_FIRST").Select(x => x.GetField<ORID>("@ORID")).ToList();
-                        break;
-                    case LimitType.DATE:
-                        var start = db.Query<ODocument>($"SELECT BlockTime AS value FROM Transaction WHERE Hash = '{_options.StartTx}'").Select(x => x.GetField<DateTime>("value")).FirstOrDefault().ToLocalTime();
-                        var end = start.AddMinutes(_options.Limit);
-                        result = db.Query<ODocument>($"SELECT * FROM Transaction WHERE BlockTime >= '{start.ToString("yyyy-MM-dd HH:mm:ss")}' AND BlockTime <= '{end.ToString("yyyy-MM-dd HH:mm:ss")}'").ToList().Select(x => x.GetField<ORID>("@ORID")).ToList();
-                        break;
-                    case LimitType.COUNT:
-                        result = db.Query<ODocument>($"TRAVERSE out() FROM (SELECT * FROM Transaction WHERE Hash = '{_options.StartTx}') LIMIT {_options.Limit} STRATEGY BREADTH_FIRST").Select(x => x.GetField<ORID>("@ORID")).ToList();
-                        break;
-                    default:
-                        result = new List<ORID>();
-                        break;
-                }
-                return result;
-            }
-        }
-
         public List<string> GetAddresses(ConnectionOptions dbOptions)
         {
             List<string> result = null;

b6577c8fbec55f79f82b61186f7e8be36180ccbc
Andreas Rothenhäuser <andreas.rothenhaeuser@fast-detect.de>
2018-10-09 12:22:31 +0200

    Implemented SocialNetwork.

---
+++ fd.Coins.Core/Clustering/Intrinsic/SocialNetwork.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using Orient.Client;
+using OrientDB_Net.binary.Innov8tive.API;
+
+namespace fd.Coins.Core.Clustering.Intrinsic
+{
+    public class SocialNetwork : Clustering
+    {
+        private Dictionary<string, List<string>> _result;
+
+        public SocialNetwork()
+        {
+            _result = new Dictionary<string, List<string>>();
+        }
+
+        public override double Distance(string addr1, string addr2)
+        {
+            var v1 = _result[addr1];
+            var v2 = _result[addr2];
+            return 1 - ((double)v1.Intersect(v2).Count() / v1.Union(v2).Count());
+        }
+
+        public override void Run(ConnectionOptions mainOptions, IEnumerable<string> addresses)
+        {
+            // DEBUG
+            Console.WriteLine($"{GetType().Name}.{MethodBase.GetCurrentMethod().Name} running...");
+            var sw = new Stopwatch();
+            sw.Start();
+            using (var mainDB = new ODatabase(mainOptions))
+            {
+                _result = mainDB.Query($"SELECT tAddr AS address, list(outV().inE().tAddr) AS payers, list(inv().outE().tAddr) AS payees FROM Link WHERE tAddr in [{string.Join(",", addresses.Select(x => "'" + x + "'"))}] GROUP BY tAddr").ToDictionary(x => x.GetField<string>("address"), y => y.GetField<List<string>>("payers").Union(y.GetField<List<string>>("payees")).ToList());
+            }
+            sw.Stop();
+            // DEBUG
+            Console.WriteLine($"{GetType().Name}.{MethodBase.GetCurrentMethod().Name} done. {sw.Elapsed}");
+        }
+    }
+}
